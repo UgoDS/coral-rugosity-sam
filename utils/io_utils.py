17067faf7a8ac72e55234163f7057843d17f8773
Ugo Piqueras <ugo.piqueras@pernod-ricard.com>
2024-02-09 09:58:00 +1100

    remove colab

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -1,7 +1,6 @@
 import os
 import shutil
 import glob
-from google.colab import output, files
 
 
 def save_uploaded_file(uploaded_file):
@@ -12,20 +11,6 @@ def save_uploaded_file(uploaded_file):
         with open(os.path.join("images", uploaded_file.name), "wb") as f:
             f.write(uploaded_file.getbuffer())
     return uploaded_file.name
-
-
-def upload_images():
-    uploaded = files.upload()
-    list_images = []
-
-    for fn in uploaded.keys():
-        os.rename(
-            f"/content/coral-rugosity-sam/{fn}",
-            f"/content/coral-rugosity-sam/images/{fn}",
-        )
-        list_images.append(f"/content/coral-rugosity-sam/images/{fn}")
-
-    return list_images
 
 
 def create_zip_file(path_folder_to_zip, path_zipped_folder):
